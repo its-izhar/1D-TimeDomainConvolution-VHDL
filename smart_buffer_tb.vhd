5e6fa71ea70a182ce843ef316c7bd80092be561c
its-izhar <izharits@gmail.com>
2016-12-04 17:39:22 -0800

    SMART_BUFFER: Modified testbench to test more inputs. Verified it.

--- smart_buffer_tb.vhd
+++ smart_buffer_tb.vhd
@@ -14,7 +14,7 @@ end sb_test;
 
 architecture sb_test of smart_buffer_tb is
 
-  constant C_NUM_OUTPUTS : positive := 3;
+  constant C_NUM_OUTPUTS : positive := 2;
   constant C_DATA_WIDTH : positive := 16;
 
   signal clk        : std_logic   := '0';
@@ -30,6 +30,44 @@ architecture sb_test of smart_buffer_tb is
 
   -- extra signals
   signal clkEn : std_logic := '1';
+
+  -- loop count for test data
+  constant C_LOOP_COUNT : integer := C_NUM_OUTPUTS;
+
+  -- define reg_array for expected inputs and outputs
+  type reg_array is array(integer range<>) of std_logic_vector(C_DATA_WIDTH-1 downto 0);
+  signal data_in_expected : reg_array(0 to C_LOOP_COUNT-1);
+  -- expected outputs
+  signal data_out_expected : std_logic_vector(C_NUM_OUTPUTS*C_DATA_WIDTH-1 downto 0) := (others=>'0');
+  signal data_out_expected_array : reg_array(0 to C_NUM_OUTPUTS-1) := (others=>(others=>'0'));
+
+  -- converts from reg_out_array to std_logic_vector
+  function vectorize(input        : reg_array;
+                     arraySize    : natural;
+                     elementWidth : positive) return std_logic_vector is
+    variable temp : std_logic_vector(arraySize*elementWidth-1 downto 0);
+  begin
+    for i in 0 to arraySize-1 loop
+      temp((i+1)*elementWidth-1 downto i*elementWidth) := input(input'left+i);
+    end loop;
+
+    return temp;
+  end function;
+
+  -- returns the expected output window from data_in inputs
+  -- it basically returns an array ...
+  function getOutputWindow(input        : reg_array;
+                           arraySize    : natural;
+                           outputSize   : positive) return reg_array is
+    variable temp : reg_array(outputSize-1 downto 0) := (others=>(others=>'0'));
+  begin
+
+    for i in 0 to outputSize-1 loop
+      temp(temp'left-i) := input(input'right-i);
+    end loop;
+
+    return temp;
+  end function;
 
 begin
 
@@ -51,87 +89,77 @@ begin
     data_out   => data_out
   );
 
-  clkEn <= '1';
   -- Starts here
   clk <= not clk and clkEn after 10 ns;
 
 process
+  variable cycle_count : integer := 0;
+  variable temp_i : integer := 0;
   begin
 
-    rst <= '1';
-    dma_status <= '1';
-    data_in <= (others => '0');
+  -- reset conditions
+  clkEn <= '1';
+  rst <= '1';
+  dma_status <= '0';
+  rd_en <= '0';
+  wr_en <= '0';
+  data_in <= std_logic_vector(to_unsigned(0, C_DATA_WIDTH));
 
-    wait for 200 ns;
-    rst <= '0';
-    dma_status <= '0';
+  --wait for some time
+  wait for 50 ns;
+  rst <= '0';
 
-    for i in 0 to 4 loop
-      wait until rising_edge(clk);
-    end loop;
+  -- wait for some time for smart_buffer to initialize properly
+  for i in 0 to 5 loop
+    wait until rising_edge(clk);
+  end loop;
 
-    -- enable reads
-    rd_en <= '1';
+  -- enable reads
+  rd_en <= '1';
 
-    -- Start testing inputs
-    data_in <= std_logic_vector(to_unsigned(1, C_DATA_WIDTH));
+  -- Start testing a range of inputs
+  for i in 1 to C_LOOP_COUNT loop
+
+    -- send data_in and enable writes and wait for a cycle
+    data_in <= std_logic_vector(to_unsigned(i, C_DATA_WIDTH));
     wr_en <= '1';
     wait until rising_edge(clk);
 
-    data_in <= std_logic_vector(to_unsigned(2, C_DATA_WIDTH));
-    wr_en <= '1';
+    --if it's the last loop; stop after writing the data
+    if (i = C_LOOP_COUNT) then
+      wr_en <= '0';
+    end if;
+
+    -- keep adding data to expected inputs array every cycle
+    data_in_expected(i-1) <= std_logic_vector(to_unsigned(i, C_DATA_WIDTH));
+
+    -- update the cycle count
+    cycle_count := cycle_count + 1;
+
+    -- check if we get the valid data out
+    if (empty = '0') then
+      data_out_expected_array <= getOutputWindow(data_in_expected, C_LOOP_COUNT, C_NUM_OUTPUTS);
+      data_out_expected <= vectorize(data_out_expected_array, C_NUM_OUTPUTS, C_DATA_WIDTH);
+      assert(data_out_expected = data_out)
+          report "Output is incorrect. The output is " &
+                    integer'image(to_integer(unsigned(data_out_expected))) &
+                    " but should be " & integer'image(to_integer(unsigned(data_out)));
+    end if;
+
+  end loop;
+
+  wait until rising_edge(clk);
+  wait until rising_edge(clk);
+  dma_status <= '1';
+
+  -- wait for some time until we update done
+  for i in 0 to 1 loop
     wait until rising_edge(clk);
-    wr_en <= '0';
-    wait until rising_edge(clk);
+  end loop;
 
-    data_in <= std_logic_vector(to_unsigned(3, C_DATA_WIDTH));
-    wr_en <= '1';
-    wait until rising_edge(clk);
-    wr_en <= '0';
-    wait until rising_edge(clk);
-
-    data_in <= std_logic_vector(to_unsigned(4, C_DATA_WIDTH));
-    wr_en <= '1';
-    wait until rising_edge(clk);
-    wr_en <= '0';
-    wait until rising_edge(clk);
-
-    data_in <= std_logic_vector(to_unsigned(5, C_DATA_WIDTH));
-    wr_en <= '1';
-    wait until rising_edge(clk);
-    wr_en <= '0';
-    wait until rising_edge(clk);
-
-    data_in <= std_logic_vector(to_unsigned(6, C_DATA_WIDTH));
-    wr_en <= '1';
-    wait until rising_edge(clk);
-    wr_en <= '0';
-
-    -- disable reads
-    rd_en <= '0';
-
-    for i in 0 to 5 loop
-      wait until rising_edge(clk);
-    end loop;
-
-    -- Just a check
-    rd_en <= '1';
-    wait until rising_edge(clk);
-    rd_en <= '0';
-
-    for i in 0 to 2 loop
-      wait until rising_edge(clk);
-    end loop;
-
-    -- indicate the dma read has been finished
-    dma_status <= '1';
-
-    for i in 0 to 2 loop
-      wait until rising_edge(clk);
-    end loop;
-
-    clkEn <= '0';
-    wait;
+  -- disable clock
+  clkEn <= '0';
+  wait;
 
   end process;
 end sb_test;
